fa13d7b3c2ca071fa80f7992818515760a4b261b
netzer0 <netzer0an@gmail.com>
2025-10-16 21:19:28 -0500

    yes

--- DBSimulation/Computing/SimulationResult.cs
+++
@@ -1,7 +0,0 @@
-namespace DBSimulation.Computing;
-public class SimulationResult(double[] intensity, double physicalPixelSize, int[] colour)
-{
-    public double[] Intensity { get; set; } = intensity;
-    public double PhysicalPixelSize { get; set; } = physicalPixelSize;
-    public int[] Colour { get; set; } = colour;
-}    
